a1195aa30a14f9a027014d8f21fd07d028dd5f29
Martin Kearn <martin.kearn@microsoft.com>
2022-06-30 17:53:53 +0100

    Creat chat and message

--- src/Tests.IntegrationTests/ClassifyMessageTests.cs
+++ src/Tests.IntegrationTests/ClassifyMessageTests.cs
@@ -1,4 +1,6 @@
 using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+using Tests.IntegrationTests.Models;
 
 namespace Tests.IntegrationTests
 {
@@ -14,10 +16,31 @@ namespace Tests.IntegrationTests
         }
 
         [Fact]
-        public void GivenValidMessageReceivesValidResponse()
+        public async Task GivenValidMessageReceivesValidResponse()
         {
-            var result = _configuration["Bot1Url"];
-            Assert.Equal("http://localhost:9012", result);
+            // Arrange
+            var bot1Url = _configuration["Bot1Url"];
+            var chatsUri = new Uri($"{bot1Url}/client-api/chats");
+            using var client = new HttpClient();
+
+            // Act
+            // 1 Create Chat
+            var chatsResponse = await client.PostAsync(chatsUri, null).ConfigureAwait(false);
+            _ = chatsResponse.EnsureSuccessStatusCode();
+            var chatsResponseContent = await chatsResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var chat = JsonSerializer.Deserialize<Chat>(chatsResponseContent);
+
+            // 2 Create Message
+            var chatMessageUri = new Uri($"{bot1Url}/client-api/chats/{chat.Id}/messages");
+            using var content = new StringContent("i want to register my child at school");
+            var chatMessageResponse = await client.PostAsync(chatMessageUri, content).ConfigureAwait(false);
+            _ = chatMessageResponse.EnsureSuccessStatusCode();
+            var chatMessageResponseContent = await chatMessageResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var chatMessage = JsonSerializer.Deserialize<ChatMessage>(chatMessageResponseContent);
+
+
+            // Assert
+            Assert.Equal("http://localhost:9012", bot1Url.ToString());
         }
     }
 }
---
+++ src/Tests.IntegrationTests/Models/ChatMessage.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Tests.IntegrationTests.Models
+{
+    public class ChatMessage
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("sentBy")]
+        public string SentBy { get; set; }
+
+        [JsonPropertyName("sendTo")]
+        public string SendTo { get; set; }
+
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+
+        [JsonPropertyName("classification")]
+        public string Classification { get; set; }
+
+        [JsonPropertyName("createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonPropertyName("modelType")]
+        public string ModelType { get; set; }
+    }
+}
