2bd5dd18889b6241bb09bd04da607bada18ff5d3
Martin Kearn <martin.kearn@microsoft.com>
2022-07-04 09:04:37 +0100

    Passing test

--- src/Tests.IntegrationTests/ClassifyMessageTests.cs
+++ src/Tests.IntegrationTests/ClassifyMessageTests.cs
@@ -25,22 +25,33 @@ namespace Tests.IntegrationTests
 
             // Act
             // 1 Create Chat
-            var chatsResponse = await client.PostAsync(chatsUri, null).ConfigureAwait(false);
-            _ = chatsResponse.EnsureSuccessStatusCode();
-            var chatsResponseContent = await chatsResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
-            var chat = JsonSerializer.Deserialize<Chat>(chatsResponseContent);
+            var createChatResponse = await client.PostAsync(chatsUri, null).ConfigureAwait(false);
+            _ = createChatResponse.EnsureSuccessStatusCode();
+            var createChatResponseContent = await createChatResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var createdChat = JsonSerializer.Deserialize<Chat>(createChatResponseContent);
 
             // 2 Create Message
-            var chatMessageUri = new Uri($"{bot1Url}/client-api/chats/{chat.Id}/messages");
+            var chatMessageUri = new Uri($"{bot1Url}/client-api/chats/{createdChat.Id}/messages");
             using var content = new StringContent("i want to register my child at school");
             var chatMessageResponse = await client.PostAsync(chatMessageUri, content).ConfigureAwait(false);
             _ = chatMessageResponse.EnsureSuccessStatusCode();
             var chatMessageResponseContent = await chatMessageResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
             var chatMessage = JsonSerializer.Deserialize<ChatMessage>(chatMessageResponseContent);
 
+            // 3 Loop or delay
+            Thread.Sleep(new TimeSpan(0, 0, 15)); // Temporary, will add proper retry loop with expanding backoff up to a timeout.
+
+            // 4 Get all chats
+            var chatsResponse = await client.GetAsync(chatsUri).ConfigureAwait(false);
+            _ = chatsResponse.EnsureSuccessStatusCode();
+            var chatsResponseContent = await chatsResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var chats = JsonSerializer.Deserialize<List<Chat>>(chatsResponseContent);
+
+            // 5 Identify this test's chat
+            var testResultChat = chats.FirstOrDefault(c => c.Id == createdChat.Id);
 
             // Assert
-            Assert.Equal("http://localhost:9012", bot1Url.ToString());
+            Assert.Equal(2, testResultChat.Messages.Count);
         }
     }
 }
