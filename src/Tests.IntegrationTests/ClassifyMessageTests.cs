bfea9bc139a0343ed15e133cb6c214ade3b9015f
Martin Kearn <martin.kearn@microsoft.com>
2022-07-04 09:13:31 +0100

    Added post helper

--- src/Tests.IntegrationTests/ClassifyMessageTests.cs
+++ src/Tests.IntegrationTests/ClassifyMessageTests.cs
@@ -21,28 +21,24 @@ namespace Tests.IntegrationTests
             // Arrange
             var bot1Url = _configuration["Bot1Url"];
             var chatsUri = new Uri($"{bot1Url}/client-api/chats");
-            using var client = new HttpClient();
 
             // Act
             // 1 Create Chat
-            var createChatResponse = await client.PostAsync(chatsUri, null).ConfigureAwait(false);
-            _ = createChatResponse.EnsureSuccessStatusCode();
-            var createChatResponseContent = await createChatResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var createChatResponseContent = await Post(chatsUri).ConfigureAwait(false);
             var createdChat = JsonSerializer.Deserialize<Chat>(createChatResponseContent);
 
             // 2 Create Message
             var chatMessageUri = new Uri($"{bot1Url}/client-api/chats/{createdChat.Id}/messages");
             using var content = new StringContent("i want to register my child at school");
-            var chatMessageResponse = await client.PostAsync(chatMessageUri, content).ConfigureAwait(false);
-            _ = chatMessageResponse.EnsureSuccessStatusCode();
-            var chatMessageResponseContent = await chatMessageResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var chatMessageResponseContent = await Post(chatMessageUri, content).ConfigureAwait(false);
             var chatMessage = JsonSerializer.Deserialize<ChatMessage>(chatMessageResponseContent);
 
             // 3 Loop or delay
             Thread.Sleep(new TimeSpan(0, 0, 15)); // Temporary, will add proper retry loop with expanding backoff up to a timeout.
 
             // 4 Get all chats
-            var chatsResponse = await client.GetAsync(chatsUri).ConfigureAwait(false);
+            using var httpClient = new HttpClient();
+            var chatsResponse = await httpClient.GetAsync(chatsUri).ConfigureAwait(false);
             _ = chatsResponse.EnsureSuccessStatusCode();
             var chatsResponseContent = await chatsResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
             var chats = JsonSerializer.Deserialize<List<Chat>>(chatsResponseContent);
@@ -52,6 +48,16 @@ namespace Tests.IntegrationTests
 
             // Assert
             Assert.Equal(2, testResultChat.Messages.Count);
+        }
+
+        private static async Task<string> Post(Uri uri, StringContent body = null)
+        {
+            // TO DO Add verb param and make it work for all requests
+            using var httpClient = new HttpClient();
+            var response = await httpClient.PostAsync(uri, body).ConfigureAwait(false);
+            _ = response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return content;
         }
     }
 }
