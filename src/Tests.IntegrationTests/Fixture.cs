2332bd6f287e4f65706fcef5315ac80c7a838a62
Martin Kearn <martin.kearn@microsoft.com>
2022-07-12 09:22:07 +0100

    Test id and creating Dmr

--- src/Tests.IntegrationTests/Fixture.cs
+++ src/Tests.IntegrationTests/Fixture.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Globalization;
+using System.Text.Json;
 using Microsoft.Extensions.Configuration;
 using Tests.IntegrationTests.Helpers;
 using Tests.IntegrationTests.Models;
@@ -8,24 +9,37 @@ namespace Tests.IntegrationTests
     public sealed class Fixture : IDisposable
     {
         private readonly IConfiguration _configuration;
+        private readonly string _testId;
 
         public Fixture(IConfiguration configuration)
         {
             // Do "global" initialization here; Only called once.
+
+            // Setup
             _configuration = configuration;
+            _testId = DateTime.UtcNow.ToString("yyyyMMddHHmmss", CultureInfo.CurrentCulture);
             using var httpClient = new HttpClient();
             var institutionsUri = new Uri($"{_configuration["CentOpsUrl"]}/admin/institutions");
             var participantsUri = new Uri($"{_configuration["CentOpsUrl"]}/admin/participants");
 
             // Create Institution
-            var institutionPostBody = new InstitutionPost()
+            var institutionPostBody = JsonSerializer.Serialize(new InstitutionPost()
             {
-                Name = "TestInstitution",
-            };
-            var institutionPostBodyString = JsonSerializer.Serialize(institutionPostBody);
-            var institution = RequestHelper.Request<Institution>(httpClient, Verb.Post, institutionsUri, _configuration["CentOpsApiKey"], institutionPostBodyString).Result;
+                Name = $"TestInstitution{_testId}",
+            });
+            var institution = RequestHelper.Request<Institution>(httpClient, Verb.Post, institutionsUri, _configuration["CentOpsApiKey"], institutionPostBody).Result;
 
             // Create Dmr
+            var dmrPostBody = JsonSerializer.Serialize(new ParticipantPost()
+            {
+                Name = $"TestDmr{_testId}",
+                InstitutionId = institution.Id,
+                Host = "http://dmr/messages",
+                Type = "Dmr",
+                Status = "Active",
+                ApiKey = "thisisareallylongkey"
+            });
+            var dmr = RequestHelper.Request<ParticipantPost>(httpClient, Verb.Post, participantsUri, _configuration["CentOpsApiKey"], dmrPostBody).Result;
         }
 
         public void Dispose()
