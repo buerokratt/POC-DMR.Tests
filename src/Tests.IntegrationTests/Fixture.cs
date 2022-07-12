c0967c9aa7e96edfba7f3f17891d1f1d304563a0
Martin Kearn <martin.kearn@microsoft.com>
2022-07-12 09:01:05 +0100

    Refactoring code to be more readable
    Fixture now creates institution

--- src/Tests.IntegrationTests/Fixture.cs
+++ src/Tests.IntegrationTests/Fixture.cs
@@ -13,22 +13,19 @@ namespace Tests.IntegrationTests
         {
             // Do "global" initialization here; Only called once.
             _configuration = configuration;
+            using var httpClient = new HttpClient();
             var institutionsUri = new Uri($"{_configuration["CentOpsUrl"]}/admin/institutions");
+            var participantsUri = new Uri($"{_configuration["CentOpsUrl"]}/admin/participants");
 
             // Create Institution
-            using var httpClient = new HttpClient();
-            var postBody = new InstitutionPost()
+            var institutionPostBody = new InstitutionPost()
             {
                 Name = "TestInstitution",
             };
-            var postBodyString = JsonSerializer.Serialize(postBody);
-            var institution = RequestHelper.Request<Institution>(
-                httpClient,
-                Verb.Post,
-                institutionsUri,
-                _configuration["CentOpsApiKey"],
-                postBodyString)
-                .Result;
+            var institutionPostBodyString = JsonSerializer.Serialize(institutionPostBody);
+            var institution = RequestHelper.Request<Institution>(httpClient, Verb.Post, institutionsUri, _configuration["CentOpsApiKey"], institutionPostBodyString).Result;
+
+            // Create Dmr
         }
 
         public void Dispose()
